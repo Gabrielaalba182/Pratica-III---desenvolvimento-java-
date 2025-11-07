ac1e73a5ee9b6b6787cd525d67d0fab9f273b430
Gabrielaalba182 <gabrielaalba75@gmail.com>
2025-11-07 16:16:10 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import { initRouter } from 'router.js';
+import { initRouter } from './router.js';
 import { setupGlobalHandlers } from './forms.js';
 
 document.addEventListener('DOMContentLoaded', () => {
